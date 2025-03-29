f56f533090439857660f933e09e2b3cef666f1b0
Krish120003 <krish120003@gmail.com>
2025-03-29 14:45:33 -0400

    feat: add badge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,14 +35,36 @@ export default function Home() {
             className="object-cover"
             fill
           />
+          <div className="absolute -right-2 top-4 z-10 rotate-12">
+            <div className="relative">
+              <div className="relative rounded-full bg-red-600 px-4 py-2 font-extrabold text-white shadow-[0_10px_20px_rgba(0,0,0,0.7)]">
+                <div className="text-center text-xs">NOW OPEN</div>
+                <div className="text-center text-lg tracking-tighter">
+                  FOR 2025
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <Image
-          src="/HeroCropped.png"
-          alt="A group of customers outside the MV'z Kitchen Food Truck in Kitchener, Ontario"
-          width={2402}
-          height={1407}
-          className="hidden w-full md:block"
-        />
+        <div className="relative">
+          <Image
+            src="/HeroCropped.png"
+            alt="A group of customers outside the MV'z Kitchen Food Truck in Kitchener, Ontario"
+            width={2402}
+            height={1407}
+            className="hidden w-full md:block"
+          />
+          <div className="absolute right-8 top-12 z-10 hidden rotate-12 md:block">
+            <div className="relative">
+              <div className="relative rounded-full bg-red-600 px-6 py-3 font-extrabold text-white shadow-[0_15px_30px_rgba(0,0,0,0.7)]">
+                <div className="text-center text-sm">NOW OPEN</div>
+                <div className="text-center text-2xl tracking-tighter">
+                  FOR 2025
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
         <p className="font-bold md:text-2xl">
           Experience the freshest Indian food in Kitchener, Waterloo, and
