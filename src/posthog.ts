a1682612ce7aa298d5987484556f586577932ecc
Krish120003 <krish120003@gmail.com>
2025-05-01 23:18:35 -0400

    refactor: use env.js for PostHog configuration

--- src/posthog.ts
+++ src/posthog.ts
@@ -1,8 +1,9 @@
 import { PostHog } from "posthog-node";
+import { env } from "~/env.js";
 
 export default function PostHogClient() {
-  const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  const posthogClient = new PostHog(env.NEXT_PUBLIC_POSTHOG_KEY, {
+    host: env.NEXT_PUBLIC_POSTHOG_HOST,
     flushAt: 1,
     flushInterval: 0,
   });
