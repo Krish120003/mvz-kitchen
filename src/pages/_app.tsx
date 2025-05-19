5aa520b82101c3a80cebfea6ed5e055998eefb3e
Krish120003 <krish120003@gmail.com>
2025-05-19 17:33:43 -0400

    feat: meta pixel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { PostHogProvider } from "posthog-js/react";
 import Head from "next/head";
 
 import "~/styles/globals.css";
+import Script from "next/script";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   useEffect(() => {
@@ -40,6 +41,32 @@ const MyApp: AppType = ({ Component, pageProps }) => {
           content="YOUR_VERIFICATION_CODE"
         />
         <link rel="icon" href="/logo.png" />
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '721072133640254');
+fbq('track', 'PageView');`,
+          }}
+        />
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=721072133640254&ev=PageView&noscript=1"
+            alt="Meta Pixel"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </Head>
       <div className={GeistSans.className}>
         <Component {...pageProps} />
